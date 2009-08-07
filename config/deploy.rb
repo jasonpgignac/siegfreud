1fb441d61a468a398b22d270102dad9175948719
Jason Gignac <jasonpgignac@gmail.com>
2009-08-07 16:01:55 -0500

    Capistrano Deploy Script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,37 @@
-set :application, "siegfreud"
+set :application, "Siegfreud"
 default_run_options[:pty] = true
-set :deploy_to, "/Volumes/Untitled/siegfreud0.2"
-set :deploy_via, :remote_cache
-
-# SCM Data
-set :scm, 'git'
+set :repository,  "git@github.com:jasonpgignac/siegfreud.git"
+set :scm, "git"
 set :scm_passphrase, "lamia6713"
-set :repository, "git@github.com:jasonpgignac/siegfreud.git"
-set :branch, 'master'
-set :git_shallow_clone, 1
-set :scm_verbose, true
-set :use_sudo, false
-
 set :user, "ptadmin"
-role :app, "ussatx-xsod-001"
+# If you have previously been relying upon the code to start, stop 
+# and restart your mongrel application, or if you rely on the database
+# migration code, please uncomment the lines you require below
+
+# If you are deploying a rails app you probably need these:
+
+#load 'ext/rails-database-migrations.rb'
+#load 'ext/rails-shared-directories.rb'
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+#load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/Volumes/Untitled/siegfreud0.2"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+# see a full list by running "gem contents capistrano | grep 'scm/'"
+
 role :web, "ussatx-xsod-001"
-role :db,  "ussatx-xsod-001", :primary => true
-
-namespace :passenger do
-  desc "Restart Application"
-  task :restart do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-end
-
-after :deploy, "passenger:restart"
+role :db, "ussatx-xsod-001", :primary => true
+role :app, "ussatx-xsod-001"
