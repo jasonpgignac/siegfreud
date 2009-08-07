7d47ebbe29958c8ff5e73521f8bb645dd8f02c47
Jason Gignac <jasonpgignac@gmail.com>
2009-08-07 16:51:19 -0500

    Changing Environment file temporarily to meet with our local server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,3 +22,6 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Line inserted temporarily to agree with our production server
+config.action_controller.relative_url_root = "/siegfreud"
