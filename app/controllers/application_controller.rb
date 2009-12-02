946465e0a15d91cc0c24466e19c9264c7e50e4d7
Jason Gignac <jasonpgignac@gmail.com>
2009-12-02 13:02:45 -0600

    Cleaned out extraneous files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,12 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include CredentialStore
-  require 'net/ldap'
-  require 'aes_crypt'
-  require 'csv'
   before_filter :authenticate
   before_filter { |c| Authorization.current_user = c.current_user }
   
   helper :all # include all helpers, all the time
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery #:secret => '052cb93edcaaa126e814e2908a5faaed'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
   filter_parameter_logging :password, :password_confirmation
-  
   helper_method :current_user
   
   # Configuration variables for authentication
